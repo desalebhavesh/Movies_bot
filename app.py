2999aa5e4e1d3205a3a85f764324ce9d289adc07
Bhavesh Desale <109806034+desalebhavesh@users.noreply.github.com>
2024-12-25 15:43:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ nltk.download('omw-1.4')
 from nltk.stem import WordNetLemmatizer
 from keras.models import load_model  # type: ignore
 
+st.write("Image path: D:\chat_bot\bot_image.jpg")
+
 
 
 # Import your sections
